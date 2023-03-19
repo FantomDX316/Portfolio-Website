4b2b810efdefc80da86c8b255d0da1dd5260ff22
saksham <phantomdx316@gmail.com>
2023-03-19 17:24:54 +0530

    added color hovering color

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
-import {Link, NavLink} from "react-router-dom";
+import React from 'react';
+import { Link, NavLink } from "react-router-dom";
 import { useLocation } from "react-router-dom"
 import "./Navbar.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome,faAddressCard, faEnvelope,faRectangleList } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faUser, faEnvelope, faRectangleList } from '@fortawesome/free-solid-svg-icons';
 import logo from "./logo.png"
 const Navbar = () => {
     const location = useLocation();
-    const active = useState("active");
-    
+
     return (
         <div className="navbar">
             <div className="logo">
@@ -17,17 +16,17 @@ const Navbar = () => {
                 </Link>
             </div>
             <nav>
-                <NavLink className={`home`} to="/">
-                    <FontAwesomeIcon icon = {faHome} size="2x" color="grey"/>
+                <NavLink className={`${location.pathname === "/" ? "active home" : "home"}`} to="/">
+                    <FontAwesomeIcon icon={faHome} size="2x" color="grey" />
                 </NavLink>
-                <NavLink className={`about`} to="/about">
-                    <FontAwesomeIcon icon = {faAddressCard } size="2x" color="grey"/>
+                <NavLink className={`${location.pathname === "/about" ? "active about" : "about"}`} to="/about">
+                    <FontAwesomeIcon icon={faUser} size="2x" color="grey" />
                 </NavLink>
-                <NavLink className={`projects`} to="/projects">
-                    <FontAwesomeIcon icon = {faRectangleList } size="2x" color="grey"/>
+                <NavLink className={`${location.pathname==="/projects"?"active projects":"projects"}`} to="/projects">
+                    <FontAwesomeIcon icon={faRectangleList} size="2x" color="grey" />
                 </NavLink>
-                <NavLink className={`contact`} to="/contact">
-                    <FontAwesomeIcon icon = {faEnvelope} size="2x" color="grey"/>
+                <NavLink className={`${location.pathname==="/contact"?"active contact":"contact"}`} to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope} size="2x" color="grey" />
                 </NavLink>
             </nav>
 
