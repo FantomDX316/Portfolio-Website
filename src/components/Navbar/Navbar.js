313983e354f4e3310b0fc3e5a3018891a268be39
saksham <phantomdx316@gmail.com>
2023-03-19 18:32:21 +0530

    added social media icons and links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,8 @@ import { Link, NavLink } from "react-router-dom";
 import { useLocation } from "react-router-dom"
 import "./Navbar.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUser, faEnvelope, faRectangleList } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faUser, faEnvelope, faRectangleList} from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faSquareGithub, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import logo from "./logo.png"
 const Navbar = () => {
     const location = useLocation();
@@ -29,6 +30,28 @@ const Navbar = () => {
                     <FontAwesomeIcon icon={faEnvelope} size="2x" color="grey" />
                 </NavLink>
             </nav>
+            <ul>
+                <li>
+                    <a className = "linkedin" target="_blank" href="https://www.linkedin.com/in/saksham-kothari-927568216/">
+                        <FontAwesomeIcon size="xl" icon ={faLinkedin}/>
+                    </a>
+                </li>
+                <li>
+                    <a className = "github" target="_blank" href="https://github.com/FantomDX316">
+                        <FontAwesomeIcon size="xl" icon ={faSquareGithub}/>
+                    </a>
+                </li>
+                <li>
+                    <a className = "twitter" target="_blank" href="https://twitter.com/sakshamyash">
+                        <FontAwesomeIcon size="xl" icon ={faTwitter}/>
+                    </a>
+                </li>
+                <li>
+                    <a className = "instagram" target="_blank" href="https://www.instagram.com/sakshamyash/?next=%2F">
+                        <FontAwesomeIcon size="xl" icon ={faInstagram}/>
+                    </a>
+                </li>
+            </ul>
 
         </div>
     )
