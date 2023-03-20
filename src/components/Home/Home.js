1dbd2025eaf1dcdef8757e84ec63fb96bb24eb2e
saksham <phantomdx316@gmail.com>
2023-03-20 10:24:40 +0530

    added s and k letters png

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import "./Home.scss";
 import sLogo from "./s.png";
 import kLogo from "./k.png";
+import sImg from "./sImg.png";
+import kImg from "./kImg.png";
 
 const Home = () => {
     return (
@@ -17,6 +19,15 @@ const Home = () => {
             <div className="lastName">
                 <img className="kLogo" src={kLogo} alt="K" />
                 <h1>othari</h1>
+            </div>
+            <div className="desc">
+                <h2>{`( Frontend & Backend Developer )`}</h2>
+            </div>
+            <div className="sImage">
+                <img src={sImg} alt="" />
+            </div>
+            <div className="kImage">
+                <img src={kImg} alt="" />
             </div>
 
         </div>
