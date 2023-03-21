78de23d3b7d9269b5c2724c1a2cee5bb05679e90
saksham <phantomdx316@gmail.com>
2023-03-21 08:50:53 +0530

    removed all stmts to create responsive design

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,45 +1,13 @@
 import React from 'react';
 import "./Home.scss";
-import sLogo from "./s.png";
-import kLogo from "./k.png";
-import sImg from "./sImg.png";
-import kImg from "./kImg.png";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLightbulb } from '@fortawesome/free-regular-svg-icons';
 
 
 const Home = () => {
     return (
-        <div className="homeComponent">
-            <div className="iam">
-
-                <h1>I'm</h1><br />
-            </div>
-            <div className="firstName">
-                <img className="sLogo" src={sLogo} alt="S" />
-                <h1>aksham</h1><br />
-            </div>
-            <div className="lastName">
-                <img className="kLogo" src={kLogo} alt="K" />
-                <h1>othari</h1>
-            </div>
-            <div className="desc">
-                <h2>{`( Frontend & Backend Developer )`}</h2>
-            </div>
-            <div className="lightBulb">
-                <FontAwesomeIcon className="bulb" size="10x" icon={faLightbulb} />
-            </div>
-            <div className="sImage">
-                <img src={sImg} alt="" />
-            </div>
-            <div className="kImage">
-                <img src={kImg} alt="" />
-            </div>
-
-
-        </div>
+        <>
+        </>
     )
-
+        
 };
 
 export default Home;
